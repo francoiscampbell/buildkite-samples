6f95aff2ef857f94712ecb89b8d28ae5ebc449d5
Francois Campbell <campbell.francois@gmail.com>
2022-04-04 11:58:40 -0400

    move echo

--- 00-bootstrap.sh
+++ 00-bootstrap.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 if [[ ! -z "$RUN_PIPELINE" ]]; then
+  echo "Running only $RUN_PIPELINE/buildkite.yml"
   buildkite-agent pipeline upload "$RUN_PIPELINE/buildkite.yml"
 else
+  echo 'Running all pipelines in this repo'
   find . -name buildkite.yml -exec buildkite-agent pipeline upload {} \;
 fi
