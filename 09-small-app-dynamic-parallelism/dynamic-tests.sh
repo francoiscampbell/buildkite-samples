b9500e22b6924b11f629c8994cd7d326c9195cab
Francois Campbell <campbell.francois@gmail.com>
2022-04-04 14:13:39 -0400

    fix path

--- 09-small-app-dynamic-parallelism/dynamic-tests.sh
+++ 09-small-app-dynamic-parallelism/dynamic-tests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-PARALLELISM=$(( ( RANDOM % 10 ) + 1 )) buildkite-agent pipeline upload tests.yml
+PARALLELISM=$(( ( RANDOM % 10 ) + 1 )) buildkite-agent pipeline upload 09-small-app-dynamic-parallelism/tests.yml
