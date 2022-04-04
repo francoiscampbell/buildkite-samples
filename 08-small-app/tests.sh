99e174a90c261c11145ee9dfe5f12b2193e4900a
Francois Campbell <campbell.francois@gmail.com>
2022-04-04 13:42:24 -0400

    add sleeps

--- 08-small-app/tests.sh
+++ 08-small-app/tests.sh
@@ -1,1 +1,1 @@
-echo "running tests branch $((BUILDKITE_PARALLEL_JOB+1))/$BUILDKITE_PARALLEL_JOB_COUNT"
+echo "running tests branch $((BUILDKITE_PARALLEL_JOB+1))/$BUILDKITE_PARALLEL_JOB_COUNT" && sleep 10
